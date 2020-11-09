449522c4f15a2504bb1b2568a8e062602ec37420
vagrant <avedj94@hotmail.com>
2020-11-09 19:14:10 +0000

    commit change

--- express_server.js
+++ express_server.js
@@ -22,27 +22,3 @@ app.get("/hello", (req,res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
-// load the things we need
-var express = require('express');
-var app = express();
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-// index page
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
-
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-
-app.listen(8080);
-console.log('8080 is the magic port');
