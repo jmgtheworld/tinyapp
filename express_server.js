886ae29b91b275a76afbd4b5640f0c47966347f0
vagrant <avedj94@hotmail.com>
2020-11-10 18:18:10 +0000

    added remove functionality

--- express_server.js
+++ express_server.js
@@ -19,9 +19,7 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL; // create shortURL:longURL key:value pair
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.redirect(`/urls/${shortURL}`);
-    
 });
 
 app.get("/urls/new", (req,res) => {
@@ -30,8 +28,19 @@ app.get("/urls/new", (req,res) => {
 
 app.get("/urls/:shortURL", (req,res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
-})
+  if (urlDatabase[req.params.shortURL]) {
+    res.render("urls_show", templateVars);
+  }
+  else {
+    const templateVars = { shortURL: req.params.shortURL + " Doesn't exist in database!", longURL: "Non exisitent shortURL"};
+    res.render("urls_show", templateVars )
+  }
+});
+
+app.post("/urls/:shortURL/delete", (req,res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
 
 app.get("/u/:shortURL", (req, res) => {
   console.log(req.params.shortURL);
@@ -44,7 +53,10 @@ app.listen(PORT, () => {
 });
 
 function generateRandomString() {
-  return Math.random().toString(36).substring(2,8);
+  let result = "";
+  let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    result += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return result;
 }
-
-// console.log(generateRandomString())
